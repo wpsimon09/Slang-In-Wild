31506c5f589a74bedbf86f8dd43bf972a1feeb92
wpsimon09 <simon.potocnak03@gmail.com>
2025-05-16 15:55:28 +0200

    added docker with sql database

---
+++ init/init.sql
@@ -0,0 +1,9 @@
+USE [master];
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.sql_logins WHERE name = 'newuser')
+BEGIN
+    CREATE LOGIN [newuser] WITH PASSWORD = '#password123sdJwnwlk', CHECK_POLICY = OFF;
+    ALTER SERVER ROLE [sysadmin] ADD MEMBER [newuser];
+END
+GO
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,30 +44,32 @@
     {#if submitFormOpened}
         <SubmitForm onCloseSubmitButton = {closeForm} onSubmit= {submitForm}/>
     {/if}
-
-        <div class="w-full bg-zinc-100 py-4 sticky top-0 z-40 md:w-[70%] lg:w-[60%] flex flex-col items-center mt-4 overflow-hidden">
-            <div class="w-full flex flex-row items-center justify-between">
-                <div class="lg:w-full flex gap-3 items-center justify-start px-4 mt-4">
-                    <input class="rounded-xl w-40 lg:w-1/3 lg:h-10" type="text" placeholder="Search">
-                    <button class="h-7 w-7 hover:h-8 hover:w-8 duration-200 hover:cursor-pointer ">
-                        <img alt="search" src="/svg/search.svg" class="h-full w-full"  />
-                    </button>
-                </div>
+    {#if !submitFormOpened}
         
-                <div class="flex justify-end  mt-4 mr-4">
-                    <button onclick={openSubmitForm} class="text-white duration-200 lg:w-40 hover:text-zinc-600 hover:cursor-pointer hover:bg-cyan-300 p-2 rounded-lg shadow-xl bg-amber-600" >Submit project</button>
-                </div>
-            </div>
-
-            
-            <h2 class="w-full text-left ml-10 lg:mt-4 mt-2">Filter by tags:</h2>
-            <div class="w-[90%] lg:w-full lg:px-4">
-                <TagView onFrontPage = {true} SlangProject={SlangProjectFilter} tagValues={Object.values(ETag)} />
-            </div>  
-        </div>
-    <div class="  md:w-[70%] lg:w-[60%] flex flex-col items-center w-full mt-4 overflow-hidden">
-
     
+    <div class="w-full bg-zinc-100 py-4 sticky top-0 z-40 md:w-[70%] lg:w-[60%] flex flex-col items-center mt-4 overflow-hidden">
+        <div class="w-full flex flex-row items-center justify-between">
+            <div class="lg:w-full flex gap-3 items-center justify-start px-4 mt-4">
+                <input class="rounded-xl w-40 lg:w-1/3 lg:h-10" type="text" placeholder="Search">
+                <button class="h-7 w-7 hover:h-8 hover:w-8 duration-200 hover:cursor-pointer ">
+                    <img alt="search" src="/svg/search.svg" class="h-full w-full"  />
+                </button>
+            </div>
+            
+            <div class="flex justify-end  mt-4 mr-4">
+                <button onclick={openSubmitForm} class="text-white duration-200 lg:w-40 hover:text-zinc-600 hover:cursor-pointer hover:bg-cyan-300 p-2 rounded-lg shadow-xl bg-amber-600" >Submit project</button>
+            </div>
+        </div>
+        
+        
+        <h2 class="w-full text-left ml-10 lg:mt-4 mt-2">Filter by tags:</h2>
+        <div class="w-[90%] lg:w-full lg:px-4">
+            <TagView onFrontPage = {true} SlangProject={SlangProjectFilter} tagValues={Object.values(ETag)} />
+        </div>  
+    </div>
+    <div class="  md:w-[70%] lg:w-[60%] flex flex-col items-center w-full mt-4 overflow-hidden">
+        
+        
         <div class="mt-2 py-5  grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3 md:gap-2 lg:gap-4 items-center h-full w-full p-2">
             {#each {length: 50} as _, i}
             <Entry num = {i} SlangProject={TestSlangProject} />
@@ -75,6 +77,7 @@
             
         </div>
     </div>
-
+    
+    {/if}
     <Footer/>
 </section>
