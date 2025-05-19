524d747f3f9aa4a8d117b54d4ddec34ecda3ce19
wpsimon09 <simon.potocnak03@gmail.com>
2025-05-19 12:46:46 +0200

    added azure adapter

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
-
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
