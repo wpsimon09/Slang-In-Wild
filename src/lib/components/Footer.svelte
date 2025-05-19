61261c23bb2b2eb247d2272f5e5ddd0b43da4f0d
wpsimon09 <simon.potocnak03@gmail.com>
2025-05-19 12:56:28 +0200

    tried to fix the build errors

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,7 +4,4 @@
 
 <footer class=" w-full justify-center h-10 lg:h-15 flex flex-row items-center bg-zinc-800">
 <p class=" text-white  opacity-50">Made by <a href="https://github.com/wpsimon09" class="underline">wpsimon09</a></p>
-<a>
-    <img src="svg/github-cyan"/>
-</a>
 </footer>
