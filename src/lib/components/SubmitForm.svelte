a2a32baa8f194d69fb0c2b69929de6e4d4dcac6b
wpsimon09 <simon.potocnak03@gmail.com>
2025-08-14 11:44:46 +0200

    added dark mode support

--- src/lib/components/SubmitForm.svelte
+++ src/lib/components/SubmitForm.svelte
@@ -1,79 +1,153 @@
 <script lang="ts">
-
-    import {createEmptySlangProject, type ISlangProject, ETag } from '$lib/EntryData';
-    import {fly, scale} from 'svelte/transition';
-    import TagView from './TagView.svelte';
+    import {
+        createEmptySlangProject,
+        type ISlangProject,
+        ETag,
+    } from "$lib/EntryData";
+    import { fly, scale } from "svelte/transition";
+    import TagView from "./TagView.svelte";
 
     //=================================
     // props
-    const {  onCloseSubmitButton } = $props();
+    const { onCloseSubmitButton } = $props();
 
     //=================================
-    // project that will be submitted 
+    // project that will be submitted
     const NewSlangProject = $state(createEmptySlangProject());
-    
+
     //================================
     // other variables
-    let serializedTags = $derived(NewSlangProject.tags.toString())
+    let serializedTags = $derived(NewSlangProject.tags.toString());
     let showSelectAtLeastOneTag = $derived(NewSlangProject.tags.length === 0);
-    let showMaxFlags = $derived(NewSlangProject.tags.length > 6)
-    let isSubmitDissabled = $derived(showMaxFlags || showSelectAtLeastOneTag  )
-
+    let showMaxFlags = $derived(NewSlangProject.tags.length > 6);
+    let isSubmitDissabled = $derived(showMaxFlags || showSelectAtLeastOneTag);
 </script>
 
-<div transition:fly="{{ y: 200,delay: 10, duration: 210 }}" class="w-screen font-chivo h-full  z-50 bg-slate-100 backdrop-blur-2xl  flex items-center justify-center ">
-    <form method="POST" class="bg-white md:w-[90%] rounded-xl shadow-lg  flex flex-col gap-4 w-full p-10 lg:p-8  lg:w-1/2 " >
-        <h1 class="text-2xl font-chivo font-semibold">Submit your project </h1>
-        <p class="opacity-70 font-chivo font-light">Your project will be reviewed, so this might take a couple of days</p>
+<div
+    transition:fly={{ y: 200, delay: 10, duration: 310 }}
+    class="w-screen font-chivo h-full z-50 bg:transparent flex items-center justify-center"
+>
+    <form
+        method="POST"
+        class="bg-white dark:bg-zinc-900 md:w-[90%] rounded-xl shadow-lg flex flex-col gap-4 w-full p-10 lg:p-8 lg:w-1/2"
+    >
+        <h1 class="text-2xl dark:text-white font-chivo font-semibold">
+            Submit your project
+        </h1>
+        <p class="opacity-70 dark:text-white font-chivo font-light">
+            Your project will be reviewed, so this might take a couple of days
+        </p>
 
         <label class="flex flex-col">
-            <span class="mb-1 font-semibold">Contact email <span class="text-red-500">*</span> </span>
-            <input name="email" autocomplete="email" class="border rounded p-2" type="email" bind:value={NewSlangProject.contactEmail} required />
-            <p class="opacity-50">your email will be used only for the review process, then it is instanly deleted </p>
+            <span class="mb-1 dark:text-white font-semibold"
+                >Contact email <span class="text-red-500">*</span>
+            </span>
+            <input
+                name="email"
+                autocomplete="email"
+                class="border rounded p-2 dark:bg-transparent dark:border-2 dark:border-zinc-300 dark:text-white"
+                type="email"
+                bind:value={NewSlangProject.contactEmail}
+                required
+            />
+            <p class="opacity-50 dark:text-white">
+                your email will be used only for the review process, then it is
+                instanly deleted
+            </p>
         </label>
 
         <label class="flex flex-col">
-            <span class="mb-1 font-semibold">Project Name <span class="text-red-500">*</span></span>
-            <input name="project-name" autocomplete="on" class="border rounded p-2" type="text" bind:value={NewSlangProject.name} required />
+            <span class="mb-1 font-semibold dark:text-white"
+                >Project Name <span class="text-red-500">*</span></span
+            >
+            <input
+                name="project-name"
+                autocomplete="on"
+                class="border rounded p-2 dark:bg-transparent dark:border-2 dark:border-zinc-300 dark:text-white"
+                type="text"
+                bind:value={NewSlangProject.name}
+                required
+            />
         </label>
 
         <label class="flex flex-col">
-            <span class="mb-1 font-semibold">Author name (can be pseudonim) <span class="text-red-500">*</span></span>
-            <input name="author-name" autocomplete="on" class="border rounded p-2" type="text" bind:value={NewSlangProject.authorName} required />
+            <span class="mb-1 font-semibold dark:text-white"
+                >Author name (can be pseudonim) <span class="text-red-500"
+                    >*</span
+                ></span
+            >
+            <input
+                name="author-name"
+                autocomplete="on"
+                class="border rounded p-2 dark:bg-transparent dark:border-2 dark:border-zinc-300 dark:text-white"
+                type="text"
+                bind:value={NewSlangProject.authorName}
+                required
+            />
         </label>
-
 
         <label class="flex flex-col">
-            <span class="mb-1 font-semibold">Project Description <span class="text-red-500">*</span> </span>
-            <textarea name="description" class="border rounded p-2" rows="3" bind:value={NewSlangProject.description} required></textarea>
+            <span class="mb-1 font-semibold dark:text-white"
+                >Project Description <span class="text-red-500">*</span>
+            </span>
+            <textarea
+                name="description"
+                class="border rounded p-2 dark:bg-transparent dark:border-2 dark:border-zinc-300 dark:text-white"
+                rows="3"
+                bind:value={NewSlangProject.description}
+                required
+            ></textarea>
         </label>
 
-        <label class="flex flex-col">
-            <span class="mb-1 font-semibold">GitHub Link <span class="text-red-500">*</span></span>
-            <input name="gh-link" class="border rounded p-2 " autocomplete="on" type="url" bind:value={NewSlangProject.ghLink} required />
+        <label class="flex flex-col dark:text-white">
+            <span class="mb-1 font-semibold"
+                >GitHub Link <span class="text-red-500">*</span></span
+            >
+            <input
+                name="gh-link"
+                class="border rounded p-2 dark:bg-transparent dark:border-2 dark:border-zinc-300 dark:text-white"
+                autocomplete="on"
+                type="url"
+                bind:value={NewSlangProject.ghLink}
+                required
+            />
         </label>
 
-        <input type="hidden" name="tags" bind:value={serializedTags}>
+        <input type="hidden" name="tags" bind:value={serializedTags} />
 
-        <span class="mb-1 font-semibold">Choose your tags:<span class="text-red-500">*</span></span>
+        <span class="mb-1 font-semibold dark:text-white"
+            >Choose your tags:<span class="text-red-500">*</span></span
+        >
 
         {#if showSelectAtLeastOneTag}
-            <p class="text-red-500 ">Select at least one tag, thank you</p>
+            <p class="text-red-500">Select at least one tag, thank you</p>
         {/if}
         {#if showMaxFlags}
-            <p class="text-red-500 ">Max flag count is 6, sorry</p>
+            <p class="text-red-500">Max flag count is 6, sorry</p>
         {/if}
         <div class="w-full overflow-auto">
-            <TagView SlangProject={NewSlangProject} onFrontPage = {false} tagValues={Object.values(ETag)}/>
+            <TagView
+                SlangProject={NewSlangProject}
+                onFrontPage={false}
+                tagValues={Object.values(ETag)}
+            />
         </div>
 
-    
-
         <div class="flex items-center w-full gap-2 mt-4">
-            <button onclick={onCloseSubmitButton} type="button" class="w-1/2 text-white duration-200  hover:cursor-pointer hover:bg-teal-700 p-2 rounded-lg shadow-xl bg-amber-600">
+            <button
+                onclick={onCloseSubmitButton}
+                type="button"
+                class="w-1/2 text-white duration-200 hover:cursor-pointer hover:bg-teal-700 p-2 rounded-lg shadow-xl bg-amber-600"
+            >
                 Close
             </button>
-            <button disabled={isSubmitDissabled} type="submit" class="{isSubmitDissabled ? "opacity-40" : ""} w-1/2 text-white duration-200  hover:cursor-pointer hover:bg-teal-700 p-2 rounded-lg shadow-xl bg-amber-600">
+            <button
+                disabled={isSubmitDissabled}
+                type="submit"
+                class="{isSubmitDissabled
+                    ? 'opacity-40'
+                    : ''} w-1/2 text-white duration-200 hover:cursor-pointer hover:bg-teal-700 p-2 rounded-lg shadow-xl bg-amber-600"
+            >
                 Submit
             </button>
         </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     import Header from "$lib/components/Header.svelte";
     import Entry from "$lib/components/Entry.svelte";
     import SubmitForm from "$lib/components/SubmitForm.svelte";
-    import {enhance} from '$app/forms'
+    import { enhance } from "$app/forms";
     import {
         createEmptySlangProject,
         ETag,
@@ -13,18 +13,17 @@
     import type { PageProps } from "./$types";
     import { scale } from "svelte/transition";
     import { expoInOut } from "svelte/easing";
+    import { onMount } from "svelte";
 
     const SlangProjectFilter = $state(createEmptySlangProject());
 
     console.log("Hello, there ! Have a nice day :) ");
-
 
     //==========================================
     // initial variables
     let { data, form }: PageProps = $props();
     let submitFormOpened = $state(false);
     let searchText = $state();
-    
 
     //==========================================
     // functions
@@ -67,21 +66,21 @@
 </script>
 
 <section
-    class="f-full bg-zinc-100 font-chivo min-h-screen flex flex-col items-center"
+    class="f-full dark:bg-zinc-800 bg-zinc-100 font-chivo min-h-screen flex flex-col items-center"
 >
     {#if !submitFormOpened}
-    
         <Header />
     {/if}
 
     {#if submitFormOpened}
-    <div class="flex-1 flex-row flex items-center">
-        <SubmitForm onCloseSubmitButton={closeForm} />
-    </div>
+        <div class="flex-1 flex-row flex items-center">
+            <SubmitForm onCloseSubmitButton={closeForm} />
+        </div>
     {/if}
     {#if !submitFormOpened}
-        <div transition:scale={{ duration: 120, delay: 0, easing: expoInOut }}
-            class="w-full bg-zinc-100/90 backdrop-blur-xl py-4 sticky top-0 z-40 md:w-[90%] lg:w-[65%] flex flex-col items-center mt-4 overflow-hidden"
+        <div
+            transition:scale={{ duration: 120, delay: 0, easing: expoInOut }}
+            class="w-full dark:bg-zinc-800/90 bg-zinc-100/90 backdrop-blur-xl py-4 sticky top-0 z-40 md:w-[90%] lg:w-[65%] flex flex-col items-center mt-4 overflow-hidden"
         >
             <div class="w-full">
                 <div
@@ -92,13 +91,15 @@
                     >
                         <input
                             bind:value={searchText}
-                            class="rounded-xl w-full md:w-[90%] md:h-10 lg:w-1/3 lg:h-10"
+                            class="rounded-xl w-full md:w-[90%] md:h-10 lg:w-1/3 lg:h-10 dark:bg-transparent dark:border-2 dark:border-zinc-300 dark:text-white"
                             type="text"
                             placeholder="Search"
                         />
                     </div>
 
-                    <div class="flex flex-row items-center w-1/3 md:justify-end lg:justify-end h-10 mt-4 mr-4">
+                    <div
+                        class="flex flex-row items-center w-1/3 md:justify-end lg:justify-end h-10 mt-4 mr-4"
+                    >
                         <button
                             onclick={openSubmitForm}
                             class="text-white text-xs text-center h-full md:text-lg lg:text-lg duration-200 md:w-40 lg:w-40 w-full hover:text-white hover:cursor-pointer hover:bg-teal-700 p-2 rounded-lg shadow-xl bg-amber-600"
@@ -109,7 +110,9 @@
 
                 <div class="w-full flex-row felx items-center">
                     <div class="w-full justify-between px-4 flex mt-4 flex-row">
-                        <h2 class="w-full text-left font-bold mt-2">
+                        <h2
+                            class="w-full dark:text-white text-left font-bold mt-2"
+                        >
                             Filter by tags:
                         </h2>
                         <button
@@ -123,7 +126,9 @@
                             />
                         </button>
                     </div>
-                    <div class="w-[90%] md:w-full lg:w-full pl-4 mt-3 md:mt-4 lg:mt-2">
+                    <div
+                        class="w-[90%] md:w-full lg:w-full pl-4 mt-3 md:mt-4 lg:mt-2"
+                    >
                         <TagView
                             onFrontPage={true}
                             SlangProject={SlangProjectFilter}
@@ -134,8 +139,8 @@
             </div>
         </div>
         <div
-        transition:scale={{ duration: 120, delay: 120, easing: expoInOut }}
-        class="md:w-[90%] lg:w-[65%] flex-1 flex flex-col items-center w-full mt-4 overflow-hidden"
+            transition:scale={{ duration: 120, delay: 120, easing: expoInOut }}
+            class="md:w-[90%] lg:w-[65%] flex-1 flex flex-col items-center w-full mt-4 overflow-hidden"
         >
             {#if FilteredProjects.length === 0}
                 <div
@@ -151,7 +156,12 @@
                     </button>
                 </div>
             {:else}
-                <div transition:scale={{ duration: 120,delay:120, easing: expoInOut }}
+                <div
+                    transition:scale={{
+                        duration: 120,
+                        delay: 120,
+                        easing: expoInOut,
+                    }}
                     class="mt-2 py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-2 lg:gap-4 items-center h-full w-full p-2"
                 >
                     {#each FilteredProjects as project, i}
